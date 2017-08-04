920a49bc97a44c1efa1123b585587463390da591
Jacy <Jacyang93@gmail.com>
2017-08-04 23:41:33 +0800

    can add recipe

--- db/migrate/20170804151003_create_recipes.rb
+++ db/migrate/20170804151003_create_recipes.rb
@@ -1,7 +1,10 @@
 class CreateRecipes < ActiveRecord::Migration[5.1]
   def change
     create_table :recipes do |t|
-
+      t.references :user, foreign_key: true
+      t.string :title
+      t.string :description
+      t.json :pictures
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804145401) do
+ActiveRecord::Schema.define(version: 20170804151003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 20170804145401) do
     t.index ["user_id"], name: "index_authentications_on_user_id"
   end
 
+  create_table "recipes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "title"
+    t.string "description"
+    t.json "pictures"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -36,4 +46,5 @@ ActiveRecord::Schema.define(version: 20170804145401) do
   end
 
   add_foreign_key "authentications", "users"
+  add_foreign_key "recipes", "users"
 end
