5b649418b53e7c039996b82f972a59390ef99fea
Jacy <Jacyang93@gmail.com>
2017-08-07 18:17:27 +0800

    added simple admin role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806183221) do
+ActiveRecord::Schema.define(version: 20170807095859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20170806183221) do
     t.json "pictures"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "access_level", default: 0
   end
 
   add_foreign_key "authentications", "users"
